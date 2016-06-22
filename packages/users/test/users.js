a5a922660d19cf7c32d6c12d854862ed1c93401b
peb7268 <peb7268@gmail.com>
2016-06-22 14:33:51 -0400

    tweaking things to use npm as a build tool and remove gulp

--- packages/users/test/users.js
+++ packages/users/test/users.js
@@ -4,6 +4,6 @@ var expect 	= chai.expect;
 describe('Array', function() {
 	it('Array.indexOf', function(){
 		expect([1,2,3].indexOf(5), 'should return -1 when the value is not present').to.equal(-1);
-		expect([1,2,3].indexOf(0), 'should return -1 when the value is not present').to.equal(0);
+		expect([1,2,3].indexOf(0), 'should return -1 when the value is not present').to.equal(-1);
 	});
 });
